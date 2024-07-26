ab4f2ea505d239fbf9b402f9d4049753172591ba
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-07-26 09:28:15 +0500

    added ankauf profile page

---
+++ app/ankauf/page.tsx
@@ -0,0 +1,131 @@
+import React from "react";
+import Image from "next/image";
+import AnkaufProfile from "@/public/assets/ankauf/ankaufsprofil.png";
+import LinkButton from "@/components/link-button";
+import { Ankaufsprofil } from "../utils/data";
+
+const Ankauf = () => {
+  return (
+    <section className=" bg-light-blue pb-[3.181rem] pt-[6.75rem] lg:pb-[6.25] lg:pt-[10.656rem]">
+      <div className="container">
+        {/* section 1 */}
+        <div className="relative mb-14 flex items-center justify-center lg:mb-[6.563rem] ">
+          <Image src={AnkaufProfile} alt="ankaufProfile" />
+
+          <div className="absolute max-w-[67.438rem] text-center lg:-mt-32">
+            <h4 className=" mb-[clamp(2px,4.4vw,5.188rem)] font-proxima-semibold text-[clamp(0.625rem,1.4vw,1.313rem)] uppercase leading-tight tracking-wide 4xl:h-[1.313rem] 4xl:leading-[1.563rem]">
+              Ankaufsprofile
+            </h4>
+            <h2 className="font-gt-book text-[clamp(0.875rem,3vw,2.188rem)] leading-[clamp(0.875rem,3.4vw,3.125rem)] tracking-tight  text-gray 4xl:h-[8.438rem]">
+              Mit Seriosität, Verbindlichkeit
+              <br /> und besten Referenzen: <br />
+              Wir kaufen Immobilien und Grundstücke.
+            </h2>
+          </div>
+        </div>
+
+        {/* section 2 */}
+
+        <div className="mx-auto my-28 max-w-[59.063rem] text-center font-gt-super sm:text-lg lg:mb-[15.063rem] lg:mt-[12.313rem] lg:text-[1.563rem] lg:leading-[2.5rem] lg:tracking-tight 4xl:h-[24.063rem]">
+          <p>
+            Was gibt es Schöneres als etwas Gutes noch besser zu machen?{" "}
+            <br className="hidden lg:block" /> Wir haben diesen Ehrgeiz.
+            Besonders, wenn es um Immobilien mit Potenzial geht.
+            <br className="hidden lg:block" /> nentdeckte Schmuckstücke mit
+            jeder Menge Patina, Sanierungsstau{" "}
+            <br className="hidden lg:block" /> oder unerschlossenen Baureserven.
+          </p>
+
+          <p className="mt-4 md:mt-10">
+            Jahrzehnte im Markt, mehr als 200 erfolgreich durchgeführte
+            Projekte.
+            <br className="hidden lg:block" /> Wir sind sicher, dass viel für
+            uns spricht, wenn es um den Ankauf{" "}
+            <br className="hidden lg:block" /> Ihrer Immobilie oder Ihres
+            Grundstücks geht. Auch, und ganz besonders, dass wir ein
+            Familienunternehmen sind. Mit kurzen Wegen und schnellen
+            Entscheidungen,
+            <br className="hidden lg:block" /> was die Abwicklung Ihres
+            Ankaufsprozesses betrifft.
+          </p>
+        </div>
+
+        {/* section 3 */}
+        <div className="flex flex-col gap-[4.5rem]">
+          {Ankaufsprofil?.map((profil, index) => (
+            <div
+              key={index}
+              className=" flex flex-col bg-white px-5 py-14 pt-[2.188rem] sm:px-10 lg:px-[78px] lg:pb-[6.25rem] 4xl:h-[1213px]"
+            >
+              <div className="mb-14 flex flex-col lg:mb-[100px] lg:flex-row">
+                <div className=" flex h-[460px] flex-1 items-center justify-center ">
+                  <div className="w-full max-w-[516px] text-center lg:-mt-24">
+                    <h4 className=" font-gt-display-light text-[12.5rem] leading-none text-light-yellow xs:text-[18.75rem] md:text-[460px] ">
+                      01
+                    </h4>
+                    <h2 className="font-gt-book text-2xl text-medium-blue xs:-mt-8 xs:text-3xl md:-mt-16 md:text-[2.813rem] md:leading-[4.813rem] ">
+                      {profil?.name}
+                    </h2>
+                  </div>
+                </div>
+
+                <div className="mt-14 w-full flex-1 space-y-5 divide-y-[1px] divide-medium-blue border-medium-blue lg:mt-[85px] lg:border-l lg:pl-[52px] xl:space-y-[38px] 4xl:max-w-[757px]">
+                  <div>
+                    <span className={`ankauf-point !mt-0`}>asset-klassen</span>
+                    <span className="ankauf-desc 4xl:h-6 ">
+                      {profil?.["asset-klaseen"]}
+                    </span>
+                  </div>
+
+                  <div>
+                    <span className={`ankauf-point 4xl:h-6`}>Standorte</span>
+                    <span className="ankauf-desc 4xl:h-[3.813rem]">
+                      {profil?.standorte}
+                    </span>
+                  </div>
+
+                  <div>
+                    <span className={`ankauf-point`}>Mikrolagen</span>
+                    <span className="ankauf-desc 4xl:h-6">
+                      {profil?.mikrolagen}
+                    </span>
+                  </div>
+
+                  <div>
+                    <span className={`ankauf-point`}>Maßnahmen</span>
+                    <span className="ankauf-desc 4xl:h-[3.813rem]">
+                      {profil?.maßnahmen}
+                    </span>
+                  </div>
+
+                  <div>
+                    <span className={`ankauf-point`}>Kriterien</span>
+                    <span className="ankauf-desc 4xl:h-[3.813rem]">
+                      {profil?.kriterien}
+                    </span>
+                  </div>
+
+                  <div>
+                    <span className={`ankauf-point`}>Investitionsvolumen</span>
+                    <span className="ankauf-desc 4xl:h-6">
+                      {profil?.investitionsvolumen}
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <LinkButton
+                as={"button"}
+                variant="sm"
+                text="download Ankaufsprofil"
+                styles="self-center"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Ankauf;
--- app/utils/data.ts
+++ app/utils/data.ts
@@ -2,20 +2,59 @@ import PetitPalais from "@/public/assets/aktuelle-projekte/petit-palais.svg";
 import PetitDisplay from "@/public/assets/aktuelle-projekte/petit-display.png";
 import Name_138 from "@/public/assets/aktuelle-projekte/138-name.svg";
 import Display_138 from "@/public/assets/aktuelle-projekte/138-display.png";
+import PetitDetailDisplay from "@/public/assets/aktuelle-projekte/petit-detail-display.png";
+import PetitPhaseImage from "@/public/assets/aktuelle-projekte/petit-phaseImage.png";
+import PetitArchitektur from "@/public/assets/aktuelle-projekte/petit-architektur.png";
+import PetitInterior from "@/public/assets/aktuelle-projekte/petit-interior.png";
+import PetitBesonderheiten from "@/public/assets/aktuelle-projekte/petit-besonderheiten.png";
 
 const aktuelleprojekte = [
   {
-    id: 1,
+    id: "petit-palais",
     projectName: PetitPalais,
     displayImage: PetitDisplay,
     units: "11 verfügbare Einheiten",
     space: "Von 112 bis 235 m2",
     type: "Classic architecture Modernly interpreted",
     area: "Hamburg Uhlenhorst",
+    detailDisplay: PetitDetailDisplay,
+    philosophie: `Die Philosophie des Petit Palais erschließt sich von ganz allein – durch die elegante Architektur, die außergewöhnlich schöne Lage an der Alster und den besonderen Lifestyle in diesem Quartier.`,
+    unitMetrics: {
+      wohnungen: 11,
+      penthouse: 1,
+      "zur-alster": "3 min",
+      stellplätze: 11,
+    },
+    phaseImage: PetitPhaseImage,
+    architektur: {
+      displayImage: PetitArchitektur,
+      title: "Die Schönheit klassischer Jugendstilarchitektur",
+      description1:
+        "Das elegante Petit Palais ist ein neu erbautes  Mehrfamilienhaus, das im schönsten | Teil des Uhlenhorster Hofwegs liegt. Mit seiner weißen Fassade, seinen dezenten | Erkern und seiner  klaren Linienführung fügt es sich perfekt in die Reihe der angrenzenden historischen Jugendstilbauten ein. Dabei zeigt es einen eigenen Charakter und ergänzt das Straßenbild um schöne Details.",
+      description2:
+        "In allen seinen acht Stockwerken treffen Charme und Weite traditioneller |  Altbauten auf den Komfort und die Funktionalität der heutigen Zeit. Freiheit bietet | auch der Ausblick aus  den Obergeschossen des Petit Palais. So präsentiert sich | ein schöner Blick ins Grüne und macht die Wechsel der Jahreszeiten unmittelbar  erlebbar. In den durch eine Fußbodenheizung gewärmten  Wohnräumen sorgt | ein hochwertiger Parkettboden für einen eleganten Charakter. Bäder aus | Naturstein erfüllen hohe Ansprüche. In einer Tiefgarage bieten 11 Stellplätze |  weiteren Komfort.",
+    },
+
+    interior: {
+      displayImage: PetitInterior,
+      title: "KLASSISCHE BAUKULTUR | modern INTERPRETIERT",
+      description1:
+        "Dekorativer Naturstein, Kiesbeläge und moderne Bodenstrahler sorgen dafür, | dass das gute Wohngefühl bereits auf dem Grundstück beginnt.",
+      description2:
+        "Es sind die kleinen Dinge, die ein Bild einzigartig machen – so auch im | Petit Palais. Vom Naturstein im Treppenhaus bis zum Fischgrätparkett in allen | Wohnungen, von den Dornbracht-Armaturen in den Badezimmern bis zum | Bauhaus-Fenstergriff, von der eleganten Außenleuchte bis zur noblen | Aufzug-Ausstattung – jedes Detail hier ist mit viel Liebe ausgewählt und | Teil eines vollkommenen Ganzen.",
+    },
+    besonderheiten: {
+      displayImage: PetitBesonderheiten,
+      title: "Das Beste | Aus zwei Epochen",
+      description1:
+        "Historische Baukultur findet sich hier charmant zitiert – in Räumen, die ihren hanseatisch | traditionellen Vorbildern in nichts nachstehen und dabei ebenso repräsentativ wie | charakteristisch sind. Kombiniert mit morderner Haustechnik ergibt sich so ein Konzept, das | qualitativ Maßstäbe setzt und das Leben hier zu etwas Besonderem macht.",
+      description2:
+        "Wer die Schönheit klassischer Jugendstilarchitektur liebt und dabei | gerne viel Grün, Wasser und urbanen Lifestyle vor der eigenen Tür | weiß, der wird sich im Petit Palais mehr als zu Hause fühlen.",
+    },
   },
 
   {
-    id: 2,
+    id: "138",
     projectName: Name_138,
     displayImage: Display_138,
     units: "24 Einheiten und 2 Penthäuser",
