def06b50914e1742dd269362c3dd429e0e7934a0
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-07-29 16:23:42 +0500

    fixed sendgrid issue

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,14 +2,14 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import sendgrid from "@sendgrid/mail";
 
-sendgrid.setApiKey(process.env.EMAIL_API_KEY!);
+sendgrid.setApiKey(process.env.NEXT_PUBLIC_EMAIL_API_KEY!);
 
 export async function POST(request: NextRequest) {
   const data = await request.json();
   try {
     const msg: any = {
-      to: process.env.SENDGRID_EMAIL_TO,
-      from: process.env.SENDGRID_EMAIL_FROM,
+      to: process.env.NEXT_PUBLIC_SENDGRID_EMAIL_TO,
+      from: process.env.NEXT_PUBLIC_SENDGRID_EMAIL_FROM,
       replyTo: data?.email,
       body: data?.message,
       subject: `Nachricht an FSG Projekte`,
