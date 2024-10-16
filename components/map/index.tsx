9af82af4f8b29cf30999f7b1548bc7d11e18a326
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-10-16 15:51:35 +0500

    updated map component

--- components/map/index.tsx
+++ components/map/index.tsx
@@ -37,8 +37,8 @@ const Map: FC<MapProps> = ({ center, title, decription }) => {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <section className="relative mb-8 md:mb-[5.625rem] 4xl:h-[85.8125rem]">
-      <div className="overflow-hidden pb-12 lg:pb-[4.938rem] ">
+    <section className="mb-8 sm:mb-[5.625rem]">
+      <div className="overflow-hidden">
         <div className="bg-gray pt-12 lg:pt-[6.25rem]">
           <div className="mx-auto max-w-[54.188rem] px-4 pb-10 text-center xs:px-7 lg:pb-[6.375rem] 2xl:px-0">
             <h2 className="font-proxima-semibold uppercase leading-none tracking-wide text-brown lg:text-[1.313rem] lg:leading-[1.5rem] 4xl:h-[1.313rem]">
@@ -56,78 +56,6 @@ const Map: FC<MapProps> = ({ center, title, decription }) => {
           className="map-container h-[25rem] xl:h-[48.25rem]"
           ref={mapContainerRef}
         />
-      </div>
-
-      <div className="absolute bottom-0 left-1/2 z-10 flex h-[5rem] w-full max-w-[56.25rem] -translate-x-1/2 justify-center rounded-lg  bg-[#AAAAAA] px-1 text-white xs:justify-normal xs:px-4 md:h-[13vw] md:px-10 4xl:h-[11.625rem]">
-        <div className="mt-5 flex items-center  justify-between gap-3 xs:w-[80%] sm:w-[70%] md:mt-10 lg:w-[90%]">
-          <div className="flex items-center gap-1 xs:gap-2 ">
-            <input
-              type="checkbox"
-              id="Galerien"
-              name="Galerien"
-              value="Galerien"
-              className="peer relative size-3 shrink-0
-                                  appearance-none rounded-sm border-[2px] border-white bg-transparent accent-white checked:appearance-auto lg:size-[1.625rem]"
-            />
-            <label
-              htmlFor="Galerien"
-              className="text-xs xs:text-sm lg:text-2xl"
-            >
-              Galerien
-            </label>
-          </div>
-
-          <div className="flex items-center gap-1 xs:gap-2 ">
-            <input
-              type="checkbox"
-              id="Galerien"
-              name="Galerien"
-              value="Galerien"
-              className="peer relative size-3 shrink-0
-                                  appearance-none rounded-sm border-[2px] border-white bg-transparent accent-white checked:appearance-auto lg:size-[1.625rem]"
-            />
-            <label
-              htmlFor="Galerien"
-              className="text-xs xs:text-sm lg:text-2xl"
-            >
-              Kultur
-            </label>
-          </div>
-
-          <div className="flex items-center gap-1 xs:gap-2 ">
-            <input
-              type="checkbox"
-              id="Galerien"
-              name="Galerien"
-              value="Galerien"
-              className="peer relative size-3 shrink-0
-                                  appearance-none rounded-sm border-[2px] border-white bg-transparent accent-white checked:appearance-auto lg:size-[1.625rem]"
-            />
-            <label
-              htmlFor="Galerien"
-              className="text-xs xs:text-sm lg:text-2xl"
-            >
-              Genuss
-            </label>
-          </div>
-
-          <div className="flex items-center gap-1 xs:gap-2 ">
-            <input
-              type="checkbox"
-              id="Galerien"
-              name="Galerien"
-              value="Galerien"
-              className="peer relative size-3 shrink-0
-                                  appearance-none rounded-sm border-[2px] border-white bg-transparent accent-white checked:appearance-auto lg:size-[1.625rem]"
-            />
-            <label
-              htmlFor="Galerien"
-              className="text-xs xs:text-sm lg:text-2xl"
-            >
-              Shopping
-            </label>
-          </div>
-        </div>
       </div>
     </section>
   );
